478f7e14894387c565903995f42822ecbf9b0252
Mihail <darkserj18@gmail.com>
2023-01-11 21:54:02 +0300

    Добавление ViewModels

---
+++ CourseWorkMain/Models/ViewModels/Stats.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseWorkMain.Models.ViewModels
+{
+    public class Stats
+    {
+        public double Median { get; set; }
+        public double Mean { get; set; }
+        public double Max { get; set; }
+        public double Min { get; set; }
+    }
+}
